28ebb12399774750a3a008e64cdb42aa9c9ae085
Pierre Glandon <pglandon78@gmail.com>
2021-05-10 12:58:10 +0200

    Ready to deploy (for real)

--- public/main.js
+++ public/main.js
@@ -11,6 +11,7 @@ function createWindow() {
     nodeIntegration: true,
     contextIsolation: false,
 } });
+  mainWindow.removeMenu();
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
